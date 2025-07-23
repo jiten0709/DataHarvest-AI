e3398109c23ab958cdee21bead8a8bd49e497d16
jiten0709 <parmar.jiten.microsoft@gmail.com>
2025-07-23 13:18:31 +0530

    Implement web scraping functionality with Selenium (BigData) and BeautifulSoup

---
+++ src/scrape.py
@@ -0,0 +1,54 @@
+import os
+from dotenv import load_dotenv
+load_dotenv('../.env')
+
+from selenium.webdriver import Remote, ChromeOptions  
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection  
+from bs4 import BeautifulSoup
+
+SBR_WEBDRIVER = os.getenv("SBR_WEBDRIVER")
+
+def scrape_website(website_uri):  
+    print('Connecting to Browser API...')  
+    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')  
+    with Remote(sbr_connection, options=ChromeOptions()) as driver:  
+        print('Connected! Navigating...')  
+        driver.get(website_uri)  
+        solve_captcha = driver.execute(
+            'executeCdpCommand',
+            {
+                'cmd': 'Captcha.waitForSolve',
+                'params': {'detectTimeout': 10000},
+            }
+        )
+        print('Captcha solve status:', solve_captcha['value']['status'])
+        print('Page loaded! Scraping content...')
+        html_content = driver.page_source
+
+        return html_content
+    
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    
+    return ""
+
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, 'html.parser')
+
+    for script_or_style in soup(['script', 'style']):
+        script_or_style.extract()
+
+    cleaned_content = soup.get_text(separator='\n')
+    cleaned_content = '\n'.join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip()
+    )
+    
+    return cleaned_content
+
+def split_dom_content(dom_content, max_length=6000):
+    return [
+        dom_content[i:i + max_length] for i in range(0, len(dom_content), max_length)
+    ]
